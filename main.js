64c88477abffc88bdb2f8c0c0bc973c9bd0f4460
yeonkyu-git <yk0924.kim@gmail.com>
2021-02-12 00:37:07 +0900

    javascript: NavbarMenu Click

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-// Make navbar transparent when it is in the top
+//1.TODO: Make navbar transparent when it is in the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;  // Element 의 요소 반환 
 
@@ -12,4 +12,20 @@ document.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove('navbar--dark');
   }
+})
+
+
+//2.TODO: Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if(link == null) {
+    return;
+  } 
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: "smooth"});
+
+
 })
